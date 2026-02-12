5659ca8a5fe9e676be791d5b14c025a6dd4ec543
Daniel Henao <henao-042001@hotmail.com>
2026-02-12 00:52:17 -0500

    Inicio, creación de las clases de tipo de cuenta, creación de los constructores y referenciación de la libreria anterior

--- libTiposCtas/clsTiposCtas.cs
+++ libTiposCtas/clsTiposCtas.cs
@@ -1,13 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using libCuentaBanc;
 
 namespace libTiposCtas
 {
-    public class clsTiposCtas
+    public class clsAhorro:clsTipo
     {
-        //Punto 4.2.1
+        #region Constructores
+        public clsAhorro()
+        {
+            intNroCta = 0;
+            strFecCreac = string.Empty;
+            intTipoDoc = 0;
+            intNroDcto = 0;
+            strTitular = string.Empty;
+            fltSaldo = 0;
+            rutaFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Datos", "ctaAhorros.txt");
+
+        }
+        public clsAhorro(int tipoDoc, int nroDoc, string titular, int tipoAhor, float porcint)
+        {
+            intTipoDoc = tipoDoc;
+            intNroDcto = nroDoc;
+            strTitular = titular;
+            intTipoAhor = tipoAhor;
+            fltporcInt = porcint;
+        }
+        #endregion
+
+        #region Atributos
+        private int intTipoAhor;
+        private float fltporcInt;
+        #endregion
+
+        #region propiedades
+
+        #endregion
+
+        #region Metodos publicos
+        public override bool Buscar(int nroCta)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Crear()
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Deposito(int nroCta, float valor)
+        {
+            throw new NotImplementedException();
+        }
+        public override bool Retiro(int nroCta, float valor)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Metodos privados
+
+        #endregion
+        
     }
 }
